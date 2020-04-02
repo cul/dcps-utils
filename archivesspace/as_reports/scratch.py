bd91cbc5d1c9e4e14139fe3c1049fccecf22079b
dwhodges2 <dwhodges@gmail.com>
2020-04-02 15:07:38 -0400

    yyyyy

--- archivesspace/as_reports/scratch.py
+++ archivesspace/as_reports/scratch.py
@@ -1,8 +1,9 @@
 import ASFunctions as asf
+import json
 from pprint import pprint
 
 asf.setServer('Prod')
 
 x = asf.getArchivalObjectByRef(2, 'a748bf2338f1983514ca0b1c72021c99')
 
-pprint(x)
+pprint(json.loads(x))
