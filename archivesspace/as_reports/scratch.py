7fe81a8c6cbab319384722f101a447bd373bab98
dwhodges2 <dwhodges@gmail.com>
2020-04-02 15:29:38 -0400

    ghgenegeg44444

--- archivesspace/as_reports/scratch.py
+++ archivesspace/as_reports/scratch.py
@@ -2,7 +2,7 @@ import ASFunctions as asf
 import json
 from pprint import pprint
 
-asf.setServer('Test')
+asf.setServer('Prod')
 
 x = asf.getArchivalObjectByRef(2, 'a748bf2338f1983514ca0b1c72021c99')
 
