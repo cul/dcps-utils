fe00a8b7f3363ebd9bf10fea859550c90d2daf0e
dwhodges2 <dwhodges@gmail.com>
2020-04-02 15:24:30 -0400

    i4i4i4i

--- archivesspace/as_reports/scratch.py
+++ archivesspace/as_reports/scratch.py
@@ -6,6 +6,15 @@ asf.setServer('Prod')
 
 x = asf.getArchivalObjectByRef(2, 'a748bf2338f1983514ca0b1c72021c99')
 
+y = json.loads(x)
+
+tc = y['instances'][0]['sub_container']['top_container']
+
+print(type(tc))
+print(tc)
+
+quit()
+
 pprint(json.loads(x))
 
 y = asf.getResponse('repositories/2/top_containers/89920')
