27c82d08967f4d23ce60c6ead497009e40575cb0
dwhodges2 <dwhodges@gmail.com>
2020-04-08 16:30:12 -0400

    tc test

--- archivesspace/as_reports/report_top_containers.py
+++ archivesspace/as_reports/report_top_containers.py
@@ -1,6 +1,7 @@
 from pymarc import MARCReader
 import requests
 import os
+import json
 from pprint import pprint
 from sheetFeeder import dataSheet
 
@@ -12,7 +13,8 @@ def main():
     # This makes sure the script can be run from any working directory and still find related files.
     my_path = os.path.dirname(__file__)
 
-    sheet_id = '1tYOXSDFlkbX_revB_ULvhmCdvKkyzpipBTkYqYXcM38'
+    # sheet_id = '1tYOXSDFlkbX_revB_ULvhmCdvKkyzpipBTkYqYXcM38'
+    sheet_id = '1e43qKYvqGQFOMxA70U59yPKPs18y-k3ohRNdU-qrTH0'  # test
 
     container_sheet = dataSheet(
         sheet_id, 'containers!A:Z')
@@ -20,54 +22,111 @@ def main():
     marc_sheet = dataSheet(
         sheet_id, 'marc!A:Z')
 
+    # The collections to process (repo, asid, bibid)
     the_ids = [[2, 5337, 6911309]]
 
-    for id in the_ids:
-        repo = id[0]
-        asid = id[1]
-        bibid = id[2]
+    container_sheet.clear()
+
+    ### MARC ###
 
     # Get and save the MARC
-    marc_path = os.path.join(my_path, 'output/' + str(bibid) + '.marc')
-
-    x = get_clio_marc(bibid)
-
-    with open(marc_path, 'wb') as f:
-        f.write(x)
 
     # Read the MARC
 
     the_heads = ['bibid', 'display_string', '876$0', '876$a', '876$p']
-
     the_rows = [the_heads]
 
-    with open(marc_path, 'rb') as fh:
-        reader = MARCReader(fh)
-        for record in reader:
-            the_099 = record.get_fields('099')
-            the_bibid = the_099[0].get_subfields('a')[0]
-            the_876s = record.get_fields('876')
+    for id in the_ids:
+        bibid = id[2]
 
-            for r in the_876s:
-                # Need to specify order of subfields explicitly
-                the_row = [
-                    r.get_subfields('3')[0],
-                    r.get_subfields('0')[0],
-                    r.get_subfields('a')[0],
-                    r.get_subfields('p')[0]
-                ]
+        x = get_clio_marc(bibid)
 
-                # print(the_row)
+        with open(marc_path, 'wb') as f:
+            f.write(x)
 
-                the_row.insert(0, the_bibid)
+        marc_path = os.path.join(my_path, 'output/' + str(bibid) + '.marc')
 
-                the_rows.append(the_row)
+        with open(marc_path, 'rb') as fh:
+            reader = MARCReader(fh)
+            for record in reader:
+                the_099 = record.get_fields('099')
+                the_bibid = the_099[0].get_subfields('a')[0]
+                the_876s = record.get_fields('876')
 
-    # print(the_rows)
-    marc_sheet.clear()
+                for r in the_876s:
+                    # Need to specify order of subfields explicitly
+                    the_row = [
+                        r.get_subfields('3')[0],
+                        r.get_subfields('0')[0],
+                        r.get_subfields('a')[0],
+                        r.get_subfields('p')[0]
+                    ]
 
-    x = marc_sheet.appendData(the_rows)
-    print(x)
+                    # print(the_row)
+
+                    the_row.insert(0, the_bibid)
+
+                    the_rows.append(the_row)
+
+        # Write results to google sheet
+        marc_sheet.clear()
+        x = marc_sheet.appendData(the_rows)
+        print(x)
+
+    ###
+    #### TOP CONTAINERS ####
+
+    the_rows = []
+
+    for id in the_ids:
+        repo = id[0]
+        asid = id[1]
+
+        the_query = '/repositories/' + \
+            str(repo) + '/resources/' + str(asid) + '/top_containers'
+
+        # list of top containers
+        the_refs = json.loads(asf.getResponse(the_query))
+
+        cnt = 0
+        for r in the_refs:
+            cnt += 1
+            print(cnt)
+            try:
+                tc = json.loads(asf.getResponse(r['ref']))
+                # print(tc)
+
+                try:
+                    bibid = tc['collection'][0]['identifier']
+                except:
+                    bibid = ''
+                try:
+                    resource = tc['collection'][0]['ref']
+                except:
+                    resource = ''
+                try:
+                    uri = tc['uri']
+                except:
+                    uri = ''
+                try:
+                    type = tc['type']
+                except:
+                    type = ''
+                try:
+                    display_string = tc['display_string']
+                except:
+                    display_string = ''
+
+                a_row = [bibid, resource, uri, type, display_string]
+                # print(a_row)
+                the_rows.append(a_row)
+            except:
+                print(r)
+
+    # Write results to google sheet
+    container_sheet.clear()
+    z = container_sheet.appendData(the_rows)
+    print(z)
 
 
 def get_clio_marc(bibid):
