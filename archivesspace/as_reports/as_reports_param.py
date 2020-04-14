ecf1c416f23aa3057fe51a9e3d66fdef0e7fd53d
dwhodges2 <dwhodges@gmail.com>
2020-04-13 21:30:30 -0400

    update validate script

--- archivesspace/as_reports/as_reports_param.py
+++ archivesspace/as_reports/as_reports_param.py
@@ -1,2 +1,3 @@
 # set to amount of time to subtract from current time to filter results for recently updated. Format: P{days}D{hours}H
-time_offset = 'P0DT30H'
+#time_offset = 'P0DT30H'
+time_offset = 'P800DT30H'
--- archivesspace/as_reports/validate_as_eads.py
+++ archivesspace/as_reports/validate_as_eads.py
@@ -1,4 +1,6 @@
+# TEST VERSION; NOT IN PRODUCTION.
 # Script to copy the latest EAD files and validate them against schema and schematron. Output is piped to a google sheet report using sheetFeeder.
+
 
 import subprocess
 import csv
@@ -75,26 +77,32 @@ def main():
     # The Google Sheet to send data to
     the_data_sheet = dataSheet(
         "1tQY9kR5YOh1e7i4dVRsl_GMxpNnUgCkb5X8qJQBAsG0", "validation!A:Z"
-    )
-    # the_data_sheet = dataSheet('198ON5qZ3MYBWPbSAopWkGE6hcUD8P-KMkWkq2qRooOY','validation!A:Z') # Test
-    the_data_sheet2 = dataSheet(
-        "198ON5qZ3MYBWPbSAopWkGE6hcUD8P-KMkWkq2qRooOY", "validation!A:Z"
-    )  # This is a dupe for other reporting
+    # )
+    # the_data_sheet = dataSheet(
+    #     '1tQY9kR5YOh1e7i4dVRsl_GMxpNnUgCkb5X8qJQBAsG0', 'test!A:Z')  # Test
+    # This is a dupe for other reporting
+    the_data_sheet2=dataSheet(
+        "198ON5qZ3MYBWPbSAopWkGE6hcUD8P-KMkWkq2qRooOY", "validation!A:Z")
+
+    # Set path to saxon processor for evaluator xslt
+    saxon_path=os.path.join(my_path, '../../resources/saxon-9.8.0.12-he.jar')
 
     # Set path to schema validator (Jing)
-    jing_path = os.path.join(
+    jing_path=os.path.join(
         my_path, "../../resources/jing-20091111/bin/jing.jar")
 
-    schema_filename = "cul_as_ead.rng"
-    schematron_filename = "cul_as_ead.sch"
-    schema_path = os.path.join(my_path, schema_filename)
-    schematron_path = os.path.join(my_path, schematron_filename)
+     schema_filename="cul_as_ead.rng"
+    # schematron_filename = "cul_as_ead.sch"
+    xslt_filename="cul_as_ead.xsl"
+    schema_path=os.path.join(my_path, schema_filename)
+    xslt_path=os.path.join(my_path, xslt_filename)
 
-    # data_folder = '/opt/dcps/archivesspace/test/ead' # for testing
-    data_folder = "/cul/cul0/ldpd/archivesspace/ead_cache"
+    data_folder="/cul/cul0/ldpd/archivesspace/ead_cache"
+    # data_folder = '/cul/cul0/ldpd/archivesspace/test/ead'  # for testing
+
 
     # Use in notification email to distinguish errors/warnings
-    icons = {
+    icons={
         "redx": "\U0000274C",  # use for parse errors
         "exclamation": "\U00002757",
         "warning": "\U000026A0\U0000FE0F",  # use for schema validation errors
@@ -102,13 +110,13 @@ def main():
     }
 
     # Load files from directory into a list
-    the_file_paths = []
+    the_file_paths=[]
     for root, dirs, files in os.walk(os.path.abspath(data_folder)):
         for file in files:
             the_file_paths.append(os.path.join(root, file))
 
     # The column heads for the report spreadsheet
-    the_heads = [
+    the_heads=[
         "bibid",
         "file",
         "well-formed?",
@@ -118,21 +126,21 @@ def main():
         "warning type",
     ]
 
-    the_results = []
+    the_results=[]
 
     the_results.append(the_heads)
 
     # counters
-    parse_errors = 0
-    validation_errors = 0
-    sch_warnings = 0
+    parse_errors=0
+    validation_errors=0
+    sch_warnings=0
 
     for a_file in the_file_paths:
-        the_file_data = []
-        file_name = a_file.split("/")[-1]
-        bibid = file_name.split("_")[-1].split(".")[0]
+        the_file_data=[]
+        file_name=a_file.split("/")[-1]
+        bibid=file_name.split("_")[-1].split(".")[0]
 
-        validation_result = util.jing_process(jing_path, a_file, schema_path)
+        validation_result=util.jing_process(jing_path, a_file, schema_path)
 
         if "fatal:" in validation_result:
             # It's a parsing error.
@@ -140,14 +148,14 @@ def main():
                 icons["redx"] + " FATAL ERROR: " +
                 file_name + " could not be parsed!"
             )
-            wf_status = False
-            validation_status = False
+            wf_status=False
+            validation_status=False
             parse_errors += 1
         else:
-            wf_status = True
+            wf_status=True
             if "error:" in validation_result:
                 # It's a validation error.
-                validation_status = False
+                validation_status=False
                 print(
                     icons["warning"]
                     + " ERROR: "
@@ -156,25 +164,27 @@ def main():
                 )
                 validation_errors += 1
             else:
-                validation_status = True
+                validation_status=True
 
         if validation_result:
-            validation_result_clean = clean_output(validation_result, incl_types=False)[
+            validation_result_clean=clean_output(validation_result, incl_types=False)[
                 0
             ]
         else:
-            validation_result_clean = validation_result
+            validation_result_clean=validation_result
 
         if wf_status == False:
-            schematron_result_clean = "-"
-            warning_types = []
+            schematron_result_clean="-"
+            warning_types=[]
 
         else:
 
-            schematron_result = util.jing_process(
-                jing_path, a_file, schematron_path)
+            # schematron_result = util.jing_process(
+            #     jing_path, a_file, schematron_path)
+            schematron_result=util.saxon_process(
+                saxon_path, a_file, xslt_path, None)
 
-            if "error:" in schematron_result:
+            if schematron_result:
                 # It's a schematron violiation.
                 if report_level == "high":
                     # Only show if required by reporting level var (use to filter out large numbers of warnings).
@@ -183,14 +193,14 @@ def main():
                 sch_warnings += 1
 
             if schematron_result:
-                x = clean_output(schematron_result, incl_types=True)
-                schematron_result_clean = x[0]
-                warning_types = x[1]
+                x=clean_output(schematron_result, incl_types=True)
+                schematron_result_clean=x[0]
+                warning_types=x[1]
             else:
-                schematron_result_clean = ""
-                warning_types = ""
+                schematron_result_clean=""
+                warning_types=""
 
-        the_file_data = [
+        the_file_data=[
             bibid,
             file_name,
             wf_status,
@@ -208,19 +218,19 @@ def main():
     the_data_sheet2.appendData(the_results)
 
     # generate log and add to log tab, if exists.
-    the_tabs = the_data_sheet.initTabs
+    the_tabs=the_data_sheet.initTabs
 
-    now2 = datetime.datetime.now()
-    end_time = str(now2)
-    my_duration = str(now2 - now1)
+    now2=datetime.datetime.now()
+    end_time=str(now2)
+    my_duration=str(now2 - now1)
 
-    the_log = (
+    the_log=(
         "EADs from "
         + data_folder
         + " evaluated by "
         + schema_filename
         + " and "
-        + schematron_filename
+        + xslt_filename
         + ". Parse errors: "
         + str(parse_errors)
         + ". Schema errors: "
@@ -237,7 +247,7 @@ def main():
     )
 
     if "log" in the_tabs:
-        log_range = "log!A:A"
+        log_range="log!A:A"
         # today = datetime.datetime.today().strftime('%c')
         dataSheet(the_data_sheet.id, log_range).appendData([[the_log]])
     else:
@@ -259,23 +269,23 @@ def main():
 
 
 def clean_output(in_str, incl_types=True):
-    out_str = in_str
+    out_str=in_str
     if incl_types == True:
         # parse diagnostics terms (error types) to new list
         # (the schematron must enclose them in {curly braces} for this to work)
-        err_types = re.findall(r"\{(.*?)\}", in_str)
-        err_types = list(set(err_types))
+        err_types=re.findall(r"\{(.*?)\}", in_str)
+        err_types=list(set(err_types))
         # remove diagnostics strings, as they have been captured above
-        out_str = re.sub(r" *\{.*?\}\n?", r"", out_str, flags=re.MULTILINE)
+        out_str=re.sub(r" *\{.*?\}", r"", out_str, flags=re.MULTILINE)
     else:
-        err_types = []
+        err_types=[]
     # remove file path from each line, leaving line number
-    out_str = re.sub(r"^ */.*?\.xml:(.*)$", r"\1", out_str, flags=re.MULTILINE)
-    # cleanup
-    out_str = re.sub(
-        r": error: (assertion failed|report):\s+", r": ", out_str, flags=re.MULTILINE
-    )
-    out_str = re.sub(r"\n$", r"", out_str)
+    # out_str = re.sub(r"^ */.*?\.xml:(.*)$", r"\1", out_str, flags=re.MULTILINE)
+    # # cleanup
+    # out_str = re.sub(
+    #     r": error: (assertion failed|report):\s+", r": ", out_str, flags=re.MULTILINE
+    # )
+    out_str=re.sub(r"\n$", r"", out_str)
     # returns a list in format [<str>,<list>]
     return [out_str, err_types]
 
