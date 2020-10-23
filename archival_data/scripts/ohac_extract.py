e8dee6d04cdcd8d8288857864b268dd9c7c70b26
dwhodges2 <dwhodges@gmail.com>
2020-10-23 17:07:07 -0400

    ohac testing

--- archival_data/scripts/ohac_extract.py
+++ archival_data/scripts/ohac_extract.py
@@ -2,22 +2,25 @@
 
 import subprocess
 import os
+import re
 
 
 def main():
-
 
     my_name = __file__
 
     # This makes sure the script can be run from any working directory and still find related files.
     my_path = os.path.dirname(__file__)
 
-
     extract_script_path = '/cul/cul0/ldpd/ccoh/fetchOralHistoryRecords'
-    marc_output_path = '/cul/cul0/ldpd/archival_data/marc/oral_history_portal/ohac_marc.xml'
     solr_output_path = '/cul/cul0/ldpd/archival_data/solr/ohac_solr.xml'
+    marc_output_clean_path = '/cul/cul0/ldpd/archival_data/marc/oral_history_portal/ohac_clean_marc.xml'
     saxon_path = os.path.join(my_path, "../../resources/saxon-9.8.0.12-he.jar")
     xslt_path = os.path.join(my_path, 'oral2solr.xsl')
+
+    if os.path.exists(marc_output_path):
+        print("Removing old file at " + marc_output_path)
+        os.remove(marc_output_path)
 
     the_shell_command = extract_script_path + ' --output ' + marc_output_path
 
@@ -25,39 +28,53 @@ def main():
 
     print(run_bash(the_shell_command))
 
+    # Do regex to remove some illegal characters. See ACFA-270.
+    sanitize_xml(marc_output_path, marc_output_clean_path)
 
     print('Transforming MARC to SOLR XML...')
 
-    x = saxon_process(saxon_path, marc_output_path,xslt_path,solr_output_path)
+    x = saxon_process(saxon_path, marc_output_clean_path,
+                      xslt_path, solr_output_path)
     print(x)
 
 
-
-
-
-
 def run_bash(cmd):
-    result = subprocess.Popen([cmd], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()
+    result = subprocess.Popen([cmd], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE, shell=True).communicate()
     print(cmd)
     # result = subprocess.check_output([cmd], shell=True)
     # return result
-    if result[1]: # error
+    if result[1]:  # error
         return 'ERROR: ' + str(result[1].decode('utf-8'))
     else:
         return result[0].decode('utf-8')
 
 
 def saxon_process(saxonPath, inFile, transformFile, outFile, theParams=' '):
-    cmd = 'java -jar ' + saxonPath + ' ' + inFile  + ' ' + transformFile + ' ' + theParams + ' ' + '--suppressXsltNamespaceCheck:on' + ' > ' + outFile
+    cmd = 'java -jar ' + saxonPath + ' ' + inFile + ' ' + transformFile + ' ' + \
+        theParams + ' ' + '--suppressXsltNamespaceCheck:on' + ' > ' + outFile
     print(cmd)
-    p = subprocess.Popen([cmd], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    p = subprocess.Popen([cmd], stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     result = p.communicate()
-    if result[1]: # error
+    if result[1]:  # error
         return 'SAXON ERROR: ' + str(result[1].decode('utf-8'))
     else:
         return result[0].decode('utf-8')
 
 
+def sanitize_xml(in_path, out_path):
+    # strip out some bad bytes that will foul up parsing.
+    with open(in_path, "r") as f:
+        content = f.read()
+        repl = re.subn(r"[\x01-\x08\x0b\x0c\x0e-\x1f]",
+                       r"?", content, flags=re.DOTALL)
+        if repl[1] > 0:
+            print("Warning: Replaced " +
+                  str(repl[1]) + " illegal characters in " + in_path)
+
+        with open(out_path, "w+") as f:
+            f.write(repl[0])
 
 
 if __name__ == '__main__':
