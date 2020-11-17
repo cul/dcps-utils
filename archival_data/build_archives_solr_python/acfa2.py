2d8dc9ec004df5b2adefe85ef62b50bdb4e26b9b
dwhodges2 <dwhodges@gmail.com>
2020-11-17 16:55:34 -0500

    test20

--- archival_data/build_archives_solr_python/acfa2.py
+++ archival_data/build_archives_solr_python/acfa2.py
@@ -20,10 +20,59 @@ def run_bash(cmd, errorPrefix=''):
     return result[0].decode('utf-8')
 
 
-def run_saxon(saxonPath, inFile, transformFile, outFile, theParams=' '):
-    cmd = 'java -jar ' + saxonPath + ' ' + inFile + ' ' + transformFile + ' ' + \
-        theParams + ' ' + '--suppressXsltNamespaceCheck:on' + ' > ' + outFile
-    return run_bash(cmd, 'SAXON ')
+# def run_saxon(saxonPath, inFile, transformFile, outFile, theParams=' '):
+#     cmd = 'java -jar ' + saxonPath + ' ' + inFile + ' ' + transformFile + ' ' + \
+#         theParams + ' ' + '--suppressXsltNamespaceCheck:on' + ' > ' + outFile
+#     return run_bash(cmd, 'SAXON ')
+
+
+def run_saxon(saxonPath, inFile, transformFile, outFile, theParams=" "):
+    # TODO: Test error parsing.
+    # Process an XSLT transformation. Use None for outFile to send to stdout.
+    if outFile:
+        outStr = " > " + outFile
+    else:
+        outStr = " "
+    cmd = (
+        "java -jar "
+        + saxonPath
+        + " "
+        + inFile
+        + " "
+        + transformFile
+        + " "
+        + theParams
+        + " "
+        + "--suppressXsltNamespaceCheck:on"
+        + outStr
+    )
+    # print(cmd)
+    p = subprocess.Popen(
+        [cmd],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+    )
+    result = p.communicate()
+    if result[1]:
+        if "error" in str(result[1].decode("utf-8")).lower():
+            # error
+            raise Exception("SAXON ERROR: " + str(result[1].decode("utf-8")))
+        elif "does not exist" in str(result[1].decode("utf-8")).lower():
+            # error
+            raise Exception("SAXON ERROR: " + str(result[1].decode("utf-8")))
+        elif "java.io" in str(result[1].decode("utf-8")).lower():
+            # error
+            raise Exception("SAXON ERROR: " + str(result[1].decode("utf-8")))
+        elif "permission denied" in str(result[1].decode("utf-8")).lower():
+            # error
+            raise Exception("SAXON ERROR: " + str(result[1].decode("utf-8")))
+        else:
+            # non-error output
+            return "SAXON MESSAGE: " + str(result[1].decode("utf-8"))
+    else:
+        return result[0].decode("utf-8")
 
 
 # def run_post(solr_xml_path, solr_update_url):
@@ -36,12 +85,11 @@ def run_post(solr_xml_path, solr_update_url):
     p = subprocess.Popen([cmd], stdin=subprocess.PIPE,
             stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     result = p.communicate()
-    if '300' not in str(result[0].decode('utf-8')) and '301' not in str(result[0].decode('utf-8')):
+    if '200' not in str(result[0].decode('utf-8')) and '201' not in str(result[0].decode('utf-8')):
         raise Exception('SOLR ERROR: Reponse other than 200/201!')
     elif 'Error' in str(result[1].decode('utf-8')):
         raise Exception('SOLR ERROR: ' +
                         fix_cr(str(result[1].decode('utf-8'))))
-    # print(result[0].decode('utf-8')) # test
     return result[0].decode('utf-8')
 
 
