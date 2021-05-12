9bf2326be1477dc27146583273a0ca262ca64278
dwhodges2 <dwhodges@gmail.com>
2021-05-11 21:18:30 -0400

    defefefe

--- archivesspace/tests/test_as_daily_check_clio.py
+++ archivesspace/tests/test_as_daily_check_clio.py
@@ -9,9 +9,10 @@ YESTERDAY = (datetime.date.today() -
              datetime.timedelta(days=1)).strftime("%Y%m%d")
 
 SOURCE_FOLDER = "/cul/cul0/ldpd/archivesspace/oai"
-SOURCE_PATH = os.path.join(SOURCE_FOLDER, YESTERDAY + ".asRaw.xml")
 
 
 def test_clio_check():
     the_date = TODAY if clio.is_after("20:01:01") else YESTERDAY
-    assert clio.check_clio(the_date, SOURCE_PATH) == True
+    xml_path = os.path.join(SOURCE_FOLDER, the_date + ".asRaw.xml")
+
+    assert clio.check_clio(the_date, xml_path) == True
