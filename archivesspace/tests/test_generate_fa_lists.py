47e76c582051ca2f51e26c2055085722bf0ffa9f
dwhodges2 <dwhodges@gmail.com>
2021-07-21 11:28:24 -0400

    test2

--- archivesspace/tests/test_generate_fa_lists.py
+++ archivesspace/tests/test_generate_fa_lists.py
@@ -1,6 +1,6 @@
 # Requires pytest. Checks basic connectivity and read functions from sample data sheet.
-# Run all tests with 'pytest --disable-pytest-warnings'.
-# If in virtual environment, use 'python -m pytest --disable-pytest-warnings'.
+# Run all tests with 'pytest --disable-pytest-warnings --tb=short'.
+# If in virtual environment, use 'python -m pytest --disable-pytest-warnings --tb=short'.
 import archivesspace.as_crons.generate_fa_lists as fa
 import os
 from datetime import date, timedelta
@@ -15,15 +15,25 @@ MY_PATH = os.path.dirname(__file__)
 
 
 STORAGE_DIR = "/cul/cul0/ldpd/archivesspace/"
+YAML_PATH = "/cul/cul0/ldpd/archival_data/bib_ids/valid_fa_bib_ids.yml"
 YEST_STR = str((date.today() - timedelta(days=1)).strftime("%Y%m%d"))
-TEST_BIBID = 12471376  # Wright drawings in Avery
+TEST_BIBID = "12471376"  # Wright drawings in Avery
+XSL_PATH = os.path.join(MY_PATH, "../xslt/generate_browse_list2.xsl")
+OAI_PATH = os.path.join(MY_PATH, STORAGE_DIR, "oai/" + YEST_STR + ".asAllRaw.xml")
 
 
 def test_extract_record_list():
     repo = "nnc-a"
-    xsl_path = os.path.join(MY_PATH, "../xslt/generate_browse_list2.xsl")
-    input_path = os.path.join(MY_PATH, STORAGE_DIR, "oai/" + YEST_STR + ".asAllRaw.xml")
-    x = fa.extract_record_list(repo, input_path, xsl_path)
+    x = fa.extract_record_list(repo, OAI_PATH, XSL_PATH)
     y = [i["bibid"] for i in x if i["bibid"] == TEST_BIBID]
     assert TEST_BIBID in y
-    assert len(y) > 20
+    assert len(x) > 20
+
+
+def test_filter_results():
+    repo = "nnc-ccoh"
+    x = fa.extract_record_list(repo, OAI_PATH, XSL_PATH)
+    x.append({"bibid": "9999999", "title": "TEST TITLE"})
+    y = fa.filter_fa_list(x, YAML_PATH)
+    print(y)
+    assert "9999999" in y
