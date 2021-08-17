36b86cb35882947fc77e250296291f96081d8cec
dwhodges2 <dwhodges@gmail.com>
2021-08-16 22:03:52 -0400

    compare2

--- archivesspace/as_tools/compare_as_oai.py
+++ archivesspace/as_tools/compare_as_oai.py
@@ -6,25 +6,51 @@ import datetime
 import dcps_utils as util
 
 
+MY_NAME = __file__
+MY_PATH = os.path.dirname(__file__)
+
+
 def main():
 
     destination_folder = "/cul/cul0/ldpd/archivesspace/test/oai"  # test
+    xslt_path = os.path.join(MY_PATH, "../xslt/as_compare_oai.xsl")
 
     # date_params = " "  # Use this to harvest all records.
-    date_params = "-u 20190101 "
+    date_params = "-u 20200101"
 
     today = datetime.date.today().strftime("%Y%m%d")
-    prod_output_path = os.path.join(destination_folder, today + ".asPROD.xml")
-    dev_output_path = os.path.join(destination_folder, today + ".asDEV.xml")
+    prod_harvest_path = os.path.join(destination_folder, today + ".asPROD.xml")
+    prod_diff_path = os.path.join(destination_folder, today + ".diff_prod.xml")
+    dev_harvest_path = os.path.join(destination_folder, today + ".asDEV.xml")
+    dev_diff_path = os.path.join(destination_folder, today + ".diff_dev.xml")
 
     # Harvest from Prod
-    print("Harvesting Prod data to " + prod_output_path)
-    util.oai_harvest(prod_output_path, server="Prod", date_params=date_params)
+    print("Harvesting Prod data to " + prod_harvest_path)
+    util.oai_harvest(prod_harvest_path, server="Prod", date_params=date_params)
 
     print(" ")
     # Harvest from Dev
-    print("Harvesting Dev data to " + dev_output_path)
-    util.oai_harvest(dev_output_path, server="Dev", date_params=date_params)
+    print("Harvesting Dev data to " + dev_harvest_path)
+    util.oai_harvest(dev_harvest_path, server="Dev", date_params=date_params)
+
+    print(" ")
+
+    saxon_params = (
+        " prodFile="
+        + prod_harvest_path
+        + " devFile="
+        + dev_harvest_path
+        + " prodOutFile="
+        + prod_diff_path
+        + " devOutFile="
+        + dev_diff_path
+    )
+
+    print(saxon_params)
+
+    print("Generating diff files with XSLT...")
+    x = util.saxon_process(xslt_path, xslt_path, outFile=None, theParams=saxon_params)
+    print(x)
 
     quit()
 
