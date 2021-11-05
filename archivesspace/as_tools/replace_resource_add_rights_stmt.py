2d22a92d2ae7b96bc8dcdcf9da4d15bfeadc9148
dwhodges2 <dwhodges@gmail.com>
2021-11-05 12:52:53 -0400

    cc0 script

--- archivesspace/as_tools/replace_resource_add_rights_stmt.py
+++ archivesspace/as_tools/replace_resource_add_rights_stmt.py
@@ -2,43 +2,55 @@
 import ASFunctions as asf
 from pprint import pprint
 import json
+from sheetFeeder import dataSheet
 
 
 def main():
+    """Bulk add metadata rights declaration value to list of resources.
+    Values are defined in https://aspace.library.columbia.edu/enumerations?id=102
+    See ACFA-298.
+    """
 
-    SERVER = "Dev"
+    SERVER = "Test"
     asf.setServer(SERVER)
+
+    sheet_id = "1LPn0WRngs7i3k07VpJ_52HDRdo7FyrzfcmJX1SVtjYE"
+    in_sheet = dataSheet(sheet_id, "batch")
+    out_sheet = dataSheet(sheet_id, "output")
 
     # The statement data to be added (if there is not already one)
     md_rights_stmt = {
         # "descriptive_note": "Test Descriptive Note.",
         "file_uri": "https://creativecommons.org/publicdomain/zero/1.0/",
         "jsonmodel_type": "metadata_rights_declaration",
-        # "last_verified_date": "2021-10-19 00:00:00 " "UTC",
         "license": "public_domain",
     }
 
-    # Testing
+    the_items = in_sheet.getData()
 
-    repo = 2
-    asid = 6337
+    the_output = []
 
-    x = add_md_rights_stmt(repo, asid, md_rights_stmt)
+    for item in the_items:
+        repo, asid = item[0], item[1]
+        result = add_md_rights_stmt(repo, asid, md_rights_stmt)
+        log = [SERVER, repo, asid, result]
+        the_output.append(log)
+
+    print(the_output)
+
+    x = out_sheet.appendData(the_output)
     print(x)
-
-    y = asf.getResource(repo, asid)
-    pprint(json.loads(y)["metadata_rights_declarations"])
-
     quit()
 
 
 def add_md_rights_stmt(repo, asid, md_rights_stmt):
+    print("Getting " + str(repo) + ":" + str(asid))
     res = json.loads(asf.getResource(repo, asid))
     if not res["metadata_rights_declarations"]:
         res["metadata_rights_declarations"].append(md_rights_stmt)
         return asf.postResource(repo, asid, json.dumps(res))
     else:
-        print(
+        return (
             "There is already a metadata_rights_declaration for "
             + str(repo)
             + ":"
